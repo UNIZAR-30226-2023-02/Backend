dc1f022185e8773958a3e086c896707564217aed
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-04-07 18:21:56 +0200

    Pruebas creando socckert game

--- trivial_project/sala/urls.py
+++ trivial_project/sala/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
+    path("<str:game_name>/", views.game, name="game"),
 ]
 
 
--- trivial_project/sala/views.py
+++ trivial_project/sala/views.py
@@ -11,6 +11,8 @@ def index(request):
 def room(request, room_name):
     return render(request, "sala/room.html", {"room_name": room_name})
 
+def game(request, game_name):
+    return render(request, "sala/game.html", {"game_name": game_name})
 
 def lobby(request,room_name):
     return render(request, 'sala/lobby.html',{'room_name': room_name})
