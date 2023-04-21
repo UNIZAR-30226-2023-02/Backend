c9bd2e363007daddf091679940705f87500192a8
sagHack <hacksimonsw@gmail.com>
2023-04-21 17:00:39 +0200

    aclarar funciones, errores

--- trivial_project/trivial_api/funciones_auxiliares.py
+++ trivial_project/trivial_api/funciones_auxiliares.py
@@ -11,18 +11,109 @@ def isascii(s):
     """Check if the characters in string s are in ASCII, U+0-U+7F."""
     return len(s) == len(s.encode())
 
+
+# Funcion que devuelve TRUE si no hay ningun error, False en caso contario
+def all_errors_empty(dict_response):
+    for key in dict_response.keys():
+        if key.startswith('error') and dict_response[key]:
+            return False
+    return True
+
+
+
+def validate_username_register(username):
+    mensaje_error = ""
+    # Check usuario
+    if not isascii(username):
+        mensaje_error = "Usuario con caracteres no permitidos"
+    elif not username:
+        mensaje_error = "El usuario no puede ser vacio"
+    elif len(username) < 1 or len(username) > 20:
+        mensaje_error = "El usuario no tiene la longitud correcta(1-20)"
+    elif Usuario.objects.filter(username=username).exists():
+        mensaje_error = "El usuario ya existe"
+    return mensaje_error
+
+def validate_password_register(password,confirm_password):
+    mensaje_error1 = ""
+    mensaje_error2 = ""
+    if not isascii(password):
+        mensaje_error1 = "Contraseña con caracteres no permitidos"
+    elif len(password) < 8:
+        mensaje_error1 = "Contraseña inferior a 8 carácteres"
+    elif password != confirm_password: # Hacer que comprueben que es la misma en el front-end
+        mensaje_error1 = "Contraseñas diferentes"
+        mensaje_error2 = "Contraseñas diferentes"
+    return mensaje_error1, mensaje_error2
+
+
+def validate_correo_register(correo):
+    mensaje_error = ""
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
+    if not correo:
+        mensaje_error = "El correo no puede estar vacio"
+    elif not re.search(regex,correo):
+        mensaje_error = "Correo no valido"
+    elif Usuario.objects.filter(correo=correo).exists():
+        mensaje_error = "El correo ya esta en uso"
+    return mensaje_error
+
+def validate_telefono(telefono):
+    mensaje_error = ""
+    # Check telefono
+    if not telefono:
+        mensaje_error = "El telefono no puede estar vacio"
+    elif not str(telefono).isnumeric():
+        mensaje_error = "Telefono no numerico"
+    elif len(str(telefono)) < 9 :
+        mensaje_error = "Telefono inferior a 9 numeros"
+    return mensaje_error
+
+def validate_fecha_nac(fecha_nac):
+    mensaje_error = ""
+    try:
+        datetime.strptime(fecha_nac, '%Y-%m-%d')
+    except ValueError:
+        mensaje_error = "Formato fecha nacimiento invalido(YYYY-MM-DD)"
+    return mensaje_error
+
+
+def validate_correo_cambiar_datos(correo,user_correo,username):
+    mensaje_error = ""
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
+    if not correo:
+        mensaje_error = "El correo no puede estar vacio"
+    elif not re.search(regex,correo):
+        mensaje_error = "Correo no valido"
+    elif user_correo and user_correo.username != username:
+        mensaje_error = "El correo ya esta en uso"
+    return mensaje_error
+
+
+def existe_usuario(username):
+    return Usuario.objects.filter(username=username).exists()
+            
+def es_amigo(username,amigo_username):
+    return Amigos.objects.filter(user1=username,user2=amigo_username).exists()
+
+def validate_sala_crear(nombre_sala):
+    mensaje_error = ""
+    if Sala.objects.filter(nombre_sala=nombre_sala).exists():
+        mensaje_error = "La sala ya existe, selecciona otro nombre"
+    elif not nombre_sala.isascii():
+        mensaje_error = "La sala tiene caracteres no permitidos"
+    return mensaje_error
+
+
+
+
+#######################
+#   FUNCIONES TOKEN   #
+#######################
+
 #Parseamos el token para solo obtener la parte que nos interesa.
 def extract_token(token):
     return token[6:]
-
-
-def get_username_by_id(user_id):
-    print(user_id)
-    user = Usuario.objects.filter(username=user_id).first() or None
-    if(user):
-        return user.username
-    else:
-        return None
 
 #Obtenemos el usuario y el token de la peticion
 def get_username_and_token(request):
@@ -42,24 +133,6 @@ def get_username_and_token(request):
         print("No se ha podido extraer el token y el usuario")
     return username,token
 
-#Obtenemos el usuario y el token de la peticion
-def get_userID_and_token(request):
-    token= request.headers['Authorization']
-    user_id = None
-    try:
-        token = extract_token(token)
-        user_token = Token.objects.filter(key=token).first() or None
-        if(user_token):
-            user_id = user_token.user_id
-            print("El usuario id es: ",user_id)
-    except:
-        print("No se ha podido extraer el token y el usuario")
-    return user_id,token
-
-#Obtenemos el token de la peticion
-def get_token(request):
-    token= request.headers['Authorization']
-    return extract_token(token)
 
 #Obtenemos el nombre de usuario dado el token
 def get_username_by_token(token):
@@ -69,12 +142,3 @@ def get_username_by_token(token):
         user = Usuario.objects.filter(username=usuario.user_id).first() or None
         username = user.username
     return username
-
-def get_userID_by_username(username):
-    user_id = None
-    user = Usuario.objects.filter(username=username).first() or None
-    if(user):
-        user_id = user.username
-    else:
-        user_id = None
-    return user_id
--- trivial_project/trivial_api/views.py
+++ trivial_project/trivial_api/views.py
@@ -19,7 +19,6 @@ from trivial_api.funciones_auxiliares import *
 
 class UsuarioLogin(APIView):
     def post(self, request):
-        any_error = 0
         dict_response = {
             'OK':"",
             'token':"",
@@ -33,16 +32,13 @@ class UsuarioLogin(APIView):
         # Comprobacion de errores
         # Busca si existe el usuario, si no existe guarda None en user
         user = Usuario.objects.filter(username=username).first() or None
-
         if user == None:
             dict_response['error_username'] = "Usuario invalido"
-            any_error = 1
-        else:
-            if not user.check_password(password):
-                dict_response['error_password'] = "Contraseña invalida"
-                any_error = 1
+        
+        if user and not user.check_password(password):
+            dict_response['error_password'] = "Contraseña invalida"
 
-        if any_error == 0:
+        if all_errors_empty(dict_response):
             #Se crea un token asociado al usuario, si es la primera vez que inicia.
             #en caso contrario obtiene dicho token
             token,_ = Token.objects.get_or_create(user=user)
@@ -56,7 +52,7 @@ class UsuarioLogin(APIView):
 
 class UsuarioRegistrar(APIView):
     def post(self, request):
-        any_error = 0
+        print("REGISTRAR");
         # Diccionario con lo que devolvera en la peticion
         dict_response = {
             'OK':"",
@@ -75,64 +71,20 @@ class UsuarioRegistrar(APIView):
         correo = request.data.get('correo')
         telefono = request.data.get('telefono')
 
-        # Check usuario
-        if not isascii(username):
-            dict_response['error_username'] = "Usuario con caracteres no permitidos"
-            any_error = 1
-        elif not username:
-            dict_response['error_username'] = "El usuario no puede ser vacio"
-            any_error = 1
-        elif len(username) < 1 or len(username) > 20:
-            dict_response['error_username'] = "El usuario no tiene la longitud correcta(1-20)"
-            any_error = 1
-        elif Usuario.objects.filter(username=username).exists():
-            # Comprobamos que no exista el usuario
-            dict_response['error_username'] = "El usuario ya existe"
-            any_error = 1
-
-        # Check contraseña
-        if not isascii(password):
-            dict_response['error_password'] = "Contraseña con caracteres no permitidos"
-            any_error = 1
-        elif len(password) < 8:
-            dict_response['error_password'] = "Contraseña inferior a 8 carácteres"
-            any_error = 1
-        elif password != confirm_password: # Hacer que comprueben que es la misma en el front-end
-            dict_response['error_password'] = "Contraseñas diferentes"
-            dict_response['error_confirm_password'] = "Contraseñas diferentes"
-            any_error = 1
-        
+        # Check username
+        dict_response["error_username"] = validate_username_register(username)
+        # Check password
+        dict_response["error_password"], \
+        dict_response["error_confirm_password"] = validate_password_register(password,confirm_password)
         # Check correo
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
-        if not correo:
-            dict_response['error_correo'] = "El correo no puede estar vacio"
-            any_error = 1
-        elif not re.search(regex,correo):
-            dict_response['error_correo'] = "Correo no valido"
-            any_error = 1
-        elif Usuario.objects.filter(correo=correo).exists():
-            dict_response['error_correo'] = "El correo ya esta en uso"
-            any_error = 1
-
+        dict_response["error_correo"] = validate_correo_register(correo)
         # Check telefono
-        if not telefono:
-            dict_response['error_telefono'] = "El telefono no puede estar vacio"
-            any_error = 1
-        elif not telefono.isnumeric():
-            dict_response['error_telefono'] = "Telefono no numerico"
-            any_error = 1
-        elif len(telefono) < 9 :
-            dict_response['error_telefono'] = "Telefono inferior a 9 numeros"
-            any_error = 1
-            
+        dict_response["error_telefono"] = validate_telefono(telefono)
         # Check fecha nacimiento
-        try:
-            datetime.strptime(fecha_nac, '%Y-%m-%d')
-        except ValueError:
-            dict_response['error_fecha_nac'] = "Formato fecha nacimiento invalido(YYYY-MM-DD)"
+        dict_response["error_fecha_nac"] = validate_fecha_nac(fecha_nac)
 
-        # Si tenemos errores
-        if any_error ==0:
+        # Si no hay errores
+        if all_errors_empty(dict_response):
             # Creamos el registro en la base de datos
             user = Usuario.objects.create(username=username, correo=correo,telefono = telefono,fecha_nac=fecha_nac,password=password)
             user.set_password(password)
@@ -149,24 +101,25 @@ class UsuarioDatos(APIView):
 
     def post(self, request):
         username, token = get_username_and_token(request)
-        any_error = 0
         dict_response = {
             'OK':"",
             'username':"",
             'correo': "",
+            'telefono':"",
             'fecha_nac': "",
             'monedas': "",
             'amigos':[],
         }
+        user = Usuario.objects.filter(username=username).first() or None
 
-        if Usuario.objects.filter(username=username).exists():
-            user = Usuario.objects.get(username=username)
+        if user:
             amigos = Amigos.objects.filter(user1=username)
             amigos2 = Amigos.objects.filter(user2=username)
             dict_response['username'] = user.username
             dict_response['correo'] = user.correo
             dict_response['fecha_nac'] = user.fecha_nac
-            dict_response['monedas'] = user.monedas    
+            dict_response['monedas'] = user.monedas   
+            dict_response['telefono'] = user.telefono  
             for amigo in amigos:
                 dict_response['amigos'].append(str(amigo.user2)) 
             for amigo in amigos2:
@@ -181,61 +134,36 @@ class UsuarioCambiarDatos(APIView):
     #Necesita la autenticazion
     permission_classes = [IsAuthenticated]
     def post(self, request):
-        #Con esto comprobamos si el usuario tiene acceso a la informacion
-        
-        any_error = 0
         dict_response = {
             'OK':"",
-            'error_username':"",
             'error_correo':"",
             'error_fecha_nac':"",
-            'error_telefono':""
+            'error_telefono':"",
         }
-        
-        username, token = get_username_and_token(request)
-        usuario_instancia = Usuario.objects.filter(username=username).first() or None
-        fecha_nac = request.data.get('fecha_nac')
+
         correo = request.data.get('correo')
         telefono = request.data.get('telefono')
-        usuario_correo = Usuario.objects.filter(correo=correo).first() or None
-        
+        fecha_nac = request.data.get('fecha_nac')
+
+        username, token = get_username_and_token(request)
+        user = Usuario.objects.filter(username=username).first() or None
+        user_correo = Usuario.objects.filter(correo=correo).first() or None
+
         # Check correo
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
-        if not correo:
-            dict_response['error_correo'] = "El correo no puede estar vacio"
-            any_error = 1
-        elif not re.search(regex,correo):
-            dict_response['error_correo'] = "Correo no valido"
-            any_error = 1
-        elif usuario_correo and usuario_correo.username != username:
-            dict_response['error_correo'] = "El correo ya esta en uso"
-            any_error = 1
-        print("Any error: " + str(any_error))
+        dict_response["error_correo"] = validate_correo_cambiar_datos(correo,user_correo,username)
         # Check telefono
-        if not telefono:
-            dict_response['error_telefono'] = "El telefono no puede estar vacio"
-            any_error = 1
-        elif not telefono.isnumeric():
-            dict_response['error_telefono'] = "Telefono no numerico"
-            any_error = 1
-        elif len(telefono) < 9 :
-            dict_response['error_telefono'] = "Telefono inferior a 9 numeros"
-            any_error = 1
-                
-        # Check fecha nacimiento
-        try:
-            datetime.strptime(fecha_nac, '%Y-%m-%d')
-        except ValueError:
-            dict_response['error_fecha_nac'] = "Formato fecha nacimiento invalido(YYYY-MM-DD)"
+        dict_response["error_telefono"] = validate_telefono(telefono)
+        dict_response["error_fecha_nac"] = validate_fecha_nac(fecha_nac)
+
 
         # Si tenemos errores
-        if any_error ==0:
+        if all_errors_empty(dict_response):
             # Creamos el registro en la base de datos
-            usuario_instancia.username = username
-            usuario_instancia.correo = correo
-            usuario_instancia.fecha_nac = fecha_nac
-            usuario_instancia.telefono = telefono
-            usuario_instancia.save()
+            user.username = username
+            user.correo = correo
+            user.fecha_nac = fecha_nac
+            user.telefono = telefono
+            user.save()
             dict_response['OK'] = "True"
         else:
             dict_response['OK'] = "False"
@@ -252,34 +180,27 @@ class UsuarioAddAmigo(APIView):
         username, token = get_username_and_token(request)
         amigo_username = request.data.get('amigo')
         
-        any_error = 0
         dict_response = {
             'OK':"",
             'error':""
         }
-        if not Usuario.objects.filter(username=username).exists():
-            any_error = 1
+
+        if not existe_usuario(username):
             dict_response['error'] = "El usuario no existe"
             
-        if not Usuario.objects.filter(username=amigo_username).exists():
-            any_error =1
+        if not existe_usuario(amigo_username):
             dict_response['error'] = "El usuario que intentas agregar no existe"
 
+        # Ordenamos alfabeticamente 
         if username > amigo_username:
-            
-            aux = username
-            username = amigo_username
-            amigo_username = aux
+            username , amigo_username = amigo_username,username
 
-        if Amigos.objects.filter(user1=username,user2=amigo_username).exists():
-            any_error = 1
+        if username == amigo_username:
+            dict_response['error'] = "El usuario no puede ser amigo de si mismo"
+        if es_amigo(username,amigo_username):
             dict_response['error'] = "Ya tienes el amigo agregado"
                 
-        if username == amigo_username:
-            any_error =1
-            dict_response['error'] = "El usuario no puede ser amigo de si mismo"
-
-        if any_error ==0:
+        if all_errors_empty(dict_response):
             usuario_instance = get_object_or_404(Usuario, username=username)
             amigo_instance = get_object_or_404(Usuario, username=amigo_username)
             amigo_db = Amigos.objects.create(user1=usuario_instance,user2=amigo_instance)
@@ -311,40 +232,32 @@ class SalaCrear(APIView):
         n_jugadores = request.data.get('n_jugadores')
         tipo_partida = request.data.get('tipo_partida')
         
-        if not password_sala:
-            tipo_sala = "Publico"
-        else:
-            tipo_sala = "Privado"
 
-        #Check nombre_sala
-        if Sala.objects.filter(nombre_sala=nombre_sala).exists():
-            any_error = 1
-            dict_response['error_nombre_sala'] = "La sala ya existe, selecciona otro nombre"
-        elif not nombre_sala.isascii():
-            any_error = 1
-            dict_response['error_nombre_sala'] = "La sala tiene caracteres no permitidos"
-            
-        #Check tipo_sala
+        if password_sala:
+            tipo_sala = "Privado"
+        else:
+            tipo_sala = "Publico"
+
+        # Check nombre_sala
+        dict_response["error_nombre_sala"] = validate_sala_crear(nombre_sala)
+
+        # Check tipo_sala
         if tipo_sala not in dict(Sala.SALA_CHOICES):
-            any_error = 1
             dict_response['error_tipo_sala'] = "Solo son validos: Publico,Privado"
 
-        #Check tipo_partida
+        # Check tipo_partida
         if tipo_partida not in dict(Sala.PARTIDA_CHOICES):
-            any_error = 1
             dict_response['error_tipo_partida'] = "Solo son validos: Clasico,Equipo,Tematico"
 
-        #Check n_jugadores
+        # Check n_jugadores
         if(int(n_jugadores) <2 or int(n_jugadores) >6):
-            any_error = 1
             dict_response['error_n_jugadores'] = "El numero de jugadores tiene que ser entre 2 y 6"
 
-        #Check tiempo_respuesta
+        # Check tiempo_respuesta
         if(int(tiempo_respuesta) <10 or int(tiempo_respuesta) >50):
-            any_error = 1
-            dict_response['error_tiempo_respuesta'] = "Tiempo de respuesta invalido(10-50)"
+            dict_response['error_tiempo_respuesta'] = "Tiempo de respuesta invalido (10-50)"
 
-        if any_error ==0:
+        if all_errors_empty(dict_response):
             usuario_instance = get_object_or_404(Usuario, username=username)
             # Creamos la sala
             sala = Sala.objects.create(nombre_sala=nombre_sala,creador_username=usuario_instance,tiempo_respuesta=tiempo_respuesta
