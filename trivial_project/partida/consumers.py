927d549d42730140fdb483328db9e9d49c0da375
cardalgo22 <carlos@orion.local>
2023-05-08 18:31:45 +0200

    Respuestas en orden aleatorio

--- trivial_project/partida/consumers.py
+++ trivial_project/partida/consumers.py
@@ -72,7 +72,7 @@ class GameConsumers(WebsocketConsumer):
         self.send(text_data=json.dumps(datos))
 
 
-    def disconnect(self, close_code):
+    def disconnect(self):
         # Hacemos que el usuario no este activo
         juega = Juega.objects.filter(id_partida=self.game_name,username=self.username).first() or None
         juega.activo = False
@@ -180,6 +180,6 @@ class GameConsumers(WebsocketConsumer):
             )
 
         else:
-            self.disconnect(0)
+            self.disconnect()
 
 
