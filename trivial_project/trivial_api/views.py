58af7a79c7dedd8226e83eade69068d73603c362
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-05-12 23:13:56 +0200

    CE

--- trivial_project/trivial_api/views.py
+++ trivial_project/trivial_api/views.py
@@ -813,26 +813,26 @@ class PartidaActiva(APIView):
             'ws_partida':'',
             'error':"",
         }
-        username, token = get_username_and_token(request)
-        user = Usuario.objects.filter(username=username).first() or None
-        juega = Juega.objects.filter(username=user).last() or None
-        if juega:
-            print("Aqui")
-            partida = Partida.objects.filter(id=juega.id_partida).first() or None
-            if(partida and not partida.terminada):
-                if partida.tipo == "Clasico":
-                    ws_partida = "/ws/partida/" + str(partida.id) + "/"
-                elif partida.tipo == "Tematico":
-                    ws_partida = "/ws/partida_tematico/" + str(partida.id) + "/"
-                dict_response['ws_partida'] = ws_partida
-            else:
-                dict_response["error"] = "No hay partidas activas"
-        else:
-            dict_response["error"] = "No hay partidas activas"
-        if all_errors_empty(dict_response):
-            dict_response["OK"] = "True"
-        else:
-            dict_response["OK"] = "False"
+        # username, token = get_username_and_token(request)
+        # user = Usuario.objects.filter(username=username).first() or None
+        # juega = Juega.objects.filter(username=user).last() or None
+        # if juega:
+        #     print("Aqui")
+        #     partida = Partida.objects.filter(id=juega.id_partida).first() or None
+        #     if(partida and not partida.terminada):
+        #         if partida.tipo == "Clasico":
+        #             ws_partida = "/ws/partida/" + str(partida.id) + "/"
+        #         elif partida.tipo == "Tematico":
+        #             ws_partida = "/ws/partida_tematico/" + str(partida.id) + "/"
+        #         dict_response['ws_partida'] = ws_partida
+        #     else:
+        #         dict_response["error"] = "No hay partidas activas"
+        # else:
+        #     dict_response["error"] = "No hay partidas activas"
+        # if all_errors_empty(dict_response):
+        #     dict_response["OK"] = "True"
+        # else:
+        #     dict_response["OK"] = "False"
         return Response(dict_response)
     
 
