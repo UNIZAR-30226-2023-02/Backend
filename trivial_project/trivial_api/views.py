a6c6e2c097eb2c9f45fe4323b57014826aa2d2b4
javiergbg3 <javiergonzalezgarrapa@gmail.com>
2023-05-13 19:43:42 +0200

    CE

--- trivial_project/trivial_api/views.py
+++ trivial_project/trivial_api/views.py
@@ -516,7 +516,6 @@ class SalaCrear(APIView):
         tipo_partida = str(request.data.get('tipo_partida'))
         tematica = str(request.data.get('tematica'))
         
-        print(username)
         user = Usuario.objects.filter(username=username).first() or None
         
         if rechazar_reconexion(user):
